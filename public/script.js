182ffd28d9547d15ff3472e1100f2383efb28825
cfdFoundation <chris.yager@cfdinvestments.com>
2024-02-27 13:40:49 -0500

    Delay Adviser Data Pull
    Delay Adviser Data Pull

--- public/script.js
+++ public/script.js
@@ -15,30 +15,35 @@ fetch("/routers/framework/redirectURL", {
           //If we have a destinationURL, redirect to it
           if (rURL.destinationurl != ""){
             window.location.replace(rURL.destinationurl);
-          } 
+          } else {
+            adviserTest();
+          }
         });
+    } else {
+      adviserTest();
     }
   });
 
+function adviserTest(){
+  fetch("/routers/api/getAdvisorList")
+    .then((response) => response.json())
+    .then((data) => {
+    data.forEach((advisor) => {
+        // Select the <template> we created in index.html
+        const cardTemplate = document.querySelector("template");
 
-fetch("/routers/api/getAdvisorList")
-  .then((response) => response.json())
-  .then((data) => {
-	data.forEach((advisor) => {
-      // Select the <template> we created in index.html
-      const cardTemplate = document.querySelector("template");
+        // Clone a copy of the template we can insert in the DOM as a real visible node
+        const card = cardTemplate.content.cloneNode(true);
 
-      // Clone a copy of the template we can insert in the DOM as a real visible node
-      const card = cardTemplate.content.cloneNode(true);
+        // Update the content of the cloned template with the employee data we queried from the backend
+        card.querySelector("name").innerText = advisor.first_name + ' ' + advisor.last_name;
+        card.querySelector("company").innerText = advisor.company_name; 
+        card.querySelector("email").innerText = advisor.company_email.toLowerCase();
 
-      // Update the content of the cloned template with the employee data we queried from the backend
-      card.querySelector("name").innerText = advisor.first_name + ' ' + advisor.last_name;
-      card.querySelector("company").innerText = advisor.company_name; 
-      card.querySelector("email").innerText = advisor.company_email.toLowerCase();
-
-      // Append the card as a child with the employee data to the <body> element on our page
-      document.body.appendChild(card);
-    });
-  }); 
+        // Append the card as a child with the employee data to the <body> element on our page
+        document.body.appendChild(card);
+      });
+    }); 
+}
   
   // v1.0.5
