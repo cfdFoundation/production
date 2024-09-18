67405cf866e1462eb126a9ee96d368e39b3083cb
cfdFoundation <chris.yager@cfdinvestments.com>
2024-09-18 09:20:28 -0400

    Cors for the API
    Cors for the API

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const cors = require("cors");
 
 const routers = require('./routes/routers');
+
+const allowedOrigins = ['www.joincfd.com', 'www.cfdcompanies.com', 'www.cfdinvestments.com', 'localhost', 'www.cfd-companies.com', 'www.cognitoforms.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
 
 app.use(express.json());
 app.use(express.static("public"));
